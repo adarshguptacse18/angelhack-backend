1628be84bab6c7d97f2e3a1323a5a161cb81c26e
Adarsh Gupta <adarshgupta.cse18@iitbhu.ac.in>
2024-05-12 14:11:32 +0530

    Separating positive and negative factors

--- services/geminiService.js
+++ services/geminiService.js
@@ -40,20 +40,34 @@ async function getExplanation(companyData) {
         },
     ];
     console.log({companyData});
-    const chat = model.startChat({
+    const positive = model.startChat({
         generationConfig,
         safetySettings,
         history: [
             {
                 role: "user",
-                parts: [{ text: "Can  you try to explain how to improve financial score for this data?\\nYou are given dataset of a company.  Where the model predicted the financial_health_score as 70 (which ranges from 0 to 100). Main key areas are age (in months), credit_score (which ranges from 0 to 900), loan_amount, loan_tenure, late_payments, month_wise_deposts, month_wise_withdrawal. Can you give the response a with a list of three strings format where only positive factors are written in bullet points? Note: the output should be list of three sentences. That's all nothing else. Output Format list of three strings. \n\n```\n{\"id\":13,\"name\":\"Major Company\",\"type\":null,\"age\":null,\"size\":null,\"user_id\":\"123\",\"pan\":\"DILSN8886O\",\"annual_revenue\":\"9417474\",\"annual_profit\":null,\"credit_score\":712,\"loan_amount\":1000000,\"loan_tenure\":10,\"late_payments\":7,\"financial_health_score\":77,\"month_wise_deposits\":{\"2017-5\":77053,\"2017-6\":0,\"2017-7\":1263503,\"2017-8\":935440,\"2017-9\":966065,\"2017-10\":822832,\"2017-11\":1123122,\"2018-0\":1141465,\"2018-1\":1084954,\"2018-2\":1139232,\"2018-3\":863808},\"month_wise_withdrawal\":{\"2017-5\":0,\"2017-6\":909,\"2017-7\":615327,\"2017-8\":1728029,\"2017-9\":680824,\"2017-10\":715428,\"2017-11\":686892,\"2018-0\":642697,\"2018-1\":1156376,\"2018-2\":1301874,\"2018-3\":650801}}\n```" }],
+                parts: [{ text: "Can  you try to explain how to improve financial score for this data?\\nYou are given dataset of a company.  Where the model predicted the financial_health_score as 70 (which ranges from 0 to 100). Main key areas are age (in months), credit_score (which ranges from 0 to 900), loan_amount, loan_tenure, late_payments, month_wise_deposts, month_wise_withdrawal. Can you give the response a with a list of three strings format where only positive factors are written in bullet points? Note: the output should be list of three sentences{point1: lorem, point2: ipsum, point3: hola}. That's all nothing else. Give output in JSON format. Don't write anything else Output Format list of three strings. company_data: " + JSON.stringify(companyData) + "\\n" }],
             },
         ],
     });
 
-    const result = await chat.sendMessage("YOUR_USER_INPUT");
-    const response = result.response;
-    return response.text();
+    const negative = model.startChat({
+        generationConfig,
+        safetySettings,
+        history: [
+            {
+                role: "user",
+                parts: [{ text: "Can  you try to explain how to improve financial score for this data?\\nYou are given dataset of a company.  Where the model predicted the financial_health_score as 70 (which ranges from 0 to 100). Main key areas are age (in months), credit_score (which ranges from 0 to 900), loan_amount, loan_tenure, late_payments, month_wise_deposts, month_wise_withdrawal. Can you give the response a with a list of three strings format where only negative factors are written in bullet points? Note: the output should be list of three sentences{point1: lorem, point2: ipsum, point3: hola}. That's all nothing else. Give output in JSON format. Don't write anything else Output Format list of three strings. company_data: " + JSON.stringify(companyData) + "\\n" }],
+            },
+        ],
+    });
+    const positiveResult = await positive.sendMessage("YOUR_USER_INPUT");
+    const negativeResult = await negative.sendMessage("YOUR_USER_INPUT");
+
+    return {
+        p: (positiveResult.response.text().replace("json", "")),
+        n: (negativeResult.response.text().replace("json", "")),
+    }
 }
 
 
